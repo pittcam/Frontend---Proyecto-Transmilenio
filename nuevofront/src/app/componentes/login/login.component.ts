a64dc218ce96e4429884bd97a8ab511256c0f152
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-11-16 19:21:20 -0500

    Versión prefinal

--- nuevofront/src/app/componentes/login/login.component.ts
+++ nuevofront/src/app/componentes/login/login.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
-import {AuthService} from '../../shared/auth.service';
-import {Role} from '../../dto/role';
+import { AuthService } from '../../shared/auth.service';
+import { Role } from '../../dto/role';
 
 @Component({
   selector: 'app-login',
@@ -24,6 +24,7 @@ export class LoginComponent {
   onSubmit() {
     if (!this.usuario || !this.password) {
       console.log('Por favor ingresa las credenciales');
+      window.alert('Por favor ingresa las credenciales');
       return;
     }
 
@@ -32,16 +33,21 @@ export class LoginComponent {
       next: (data) => {
         console.log('Después de auth service...');
         console.log(data);
-        if (data === 'ADMIN' || data === 'COORDINADOR') {
+        if (data === 'ADMIN' ) {
           this.router.navigate(['/dashboard']);
           console.log('Login admin');
         } else if (data === 'USER') {
           this.router.navigate(['/usuario']);
           console.log('Login usuario');
         }
+        else if (data === 'COORDINADOR') {
+          this.router.navigate(['/rutas']);
+          console.log('Login admin');
+        }
       },
-      error: () => {
+      error: (err) => {
         console.log('Login incorrecto');
+        window.alert('Usuario o contraseña incorrectos');
       }
     });
   }
